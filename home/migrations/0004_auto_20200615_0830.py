54dfc1d43d50972d3560d2cddb3f6b52b7a8ccce
Crowdbotics <team@crowdbotics.com>
2020-06-16 06:00:37 +0000

    Update models

--- home/migrations/0004_auto_20200615_0830.py
+++ home/migrations/0004_auto_20200615_0830.py
@@ -6,28 +6,13 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0003_auto_20200615_0828'),
+        ("home", "0003_auto_20200615_0828"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='customtext',
-            name='bvfjghfghfj',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='ghfjghfjhgfj',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='hfkhgfhkjhg',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='hgfjhgfjh',
-        ),
-        migrations.RemoveField(
-            model_name='customtext',
-            name='khgfjhgfkhgf',
-        ),
+        migrations.RemoveField(model_name="customtext", name="bvfjghfghfj",),
+        migrations.RemoveField(model_name="customtext", name="ghfjghfjhgfj",),
+        migrations.RemoveField(model_name="customtext", name="hfkhgfhkjhg",),
+        migrations.RemoveField(model_name="customtext", name="hgfjhgfjh",),
+        migrations.RemoveField(model_name="customtext", name="khgfjhgfkhgf",),
     ]
